305e9472cfa94a4b76b3c36317638e14633f307b
Francisco Arenas <wook82@gmail.com>
2021-05-31 18:53:43 -0400

    curso de closure and scope

---
+++ platzi_html/scope-closure/scope/function.js
@@ -0,0 +1,17 @@
+const fruits = () => {
+	var fruits = 'apple';
+	console.log(fruits);
+}
+
+fruits();
+
+const anotherFunction = () => {
+	var x = 1;
+	var x = 2;
+	let y = 1;
+	y = 2; 
+	console.log(x);
+	console.log(y);
+}
+
+anotherFunction();

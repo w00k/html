b3788b30b91ece8dea34d3d92a1eda0a2941a60b
Francisco Arenas <wook82@gmail.com>
2021-06-14 16:31:41 -0400

    add files from platzi

---
+++ platzi_html/scope-closure/debug/index.js
@@ -0,0 +1,12 @@
+var a = 'Hello';
+
+function hello(){
+  let b = 'Hello World';
+  const b = 'Hello World!';
+  if (true) {
+    let d = 'Hello World!!';
+    debugger
+  }
+}
+
+hello();
---
+++ platzi_html/scope-closure/hoisting/index.js
@@ -0,0 +1,9 @@
+a = 2;
+var a;
+console.log(a); // 2
+
+nameOfDog("Dolly");
+
+function nameOfDog(name) {
+  console.log(name);
+}

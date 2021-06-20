6f2a1b9c385e08d89e28f6f14957c1805aa91dc8
Francisco Arenas <wook82@gmail.com>
2021-06-19 20:24:30 -0400

    callback hell, para la llamada hacia API Rick And Morty

--- platzi_html/asincronismos/src/callback/challenge.js
+++ platzi_html/asincronismos/src/callback/challenge.js
@@ -1,9 +1,10 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
   let xhttp = new XMLHttpRequest();
   xhttp.open('GET', url_api, true); // true for async
-  xhttp.onreadystatechange = function (event) {
+  xhttp.onreadystatechange = (event) => {
     if (xhttp.readyState === 4 ) {
       if (xhttp.status === 200) {
 	callback(null, JSON.parse(xhttp.responseText));
@@ -13,5 +14,25 @@ function fetchData(url_api, callback) {
       }
     } 
   }
-  xhttp.send()
+  xhttp.send();
 }
+
+//callback hell. 
+//cuidado con usar esto, usar demaciadas llamadas de este tipo, es mejor redefinir las llamdas. 
+
+//obtiene lista de personajes
+fetchData(API, (error1, data1) => {
+   if (error1) return console.error(error1);
+   //hace una consulta al API concatenando el id de posición 0, id = 1
+   fetchData(API + data1.results[0].id, (error2, data2) => {
+      if (error2) return console.error(error2);
+      //hace la llamda al valor ubicado en data2.origin.url = 'https://rickandmortyapi.com/api/location/1' 
+      fetchData(data2.origin.url, (error3, data3) => {
+	 if (error3) return console.error(error3);
+	 //imprime en consola la data
+	 console.log(data1.info.count);
+	 console.log(data2.name);
+	 console.log(data3.dimension);
+      })
+   })
+});
